cc90e6c9c9f8b8526a6d2abfad4b07835ad92bb6
arthurpex <Arthurpex@gmail.com>
2022-03-08 20:19:48 -0300

    fix parser phrases

--- parser.py
+++ parser.py
@@ -17,125 +17,94 @@ tokens = lexer.tokens
 # )
 
 # Funções que definem a predecencia
+
 def p_PROGRAM(p):
-    """
-    PROGRAM: (STATEMENT | FUNCLIST)?
-    """
+    """PROGRAM : '(' STATEMENT '|' FUNCLIST ')' '?' """
+    pass
 
 def p_FUNLIST(p):
-    """
-    FUNCLIST: FUNCDEF FUNCLIST | FUNCDEF
-    """
-
+    """FUNCLIST : FUNCDEF FUNCLIST '|' FUNCDEF"""
+    pass
+#
 def p_FUNCDEF(p):
-    """
-    FUNCDEF: DEF IDENT OPEN_PAREN PARAMLIST CLOSE_PAREN OPEN_CURLY_BRACKET STATELIST CLOSE_CURLY_BRACKET
-    """
+    'FUNCDEF : def IDENT OPEN_PAREN PARAMLIST CLOSE_PAREN OPEN_CURLY_BRACKET STATELIST CLOSE_CURLY_BRACKET'
+    pass
 
 def p_PARAMLIST(p):
-    """
-    PARAMLIST: (( int | float | string ) IDENT COMMA PARAMLIST | ( int | float | string ) IDENT)?
-    """
+    """PARAMLIST : '(' '(' int '|' float '|' string ')' IDENT COMMA PARAMLIST '|' '(' int '|' float '|' string ')' IDENT ')' '?' """
+    pass
 
 def p_STATEMENT(p):
-    """
-    STATEMENT:
-            (VARDECL SEMICOLON |
-            ATRIBSTAT SEMICOLON |
-            PRINTSTAT SEMICOLON |
-            READSTAT SEMICOLON |
-            RETURNSTAT SEMICOLON |
-            IFSTAT |
-            FORSTAT |
-            OPEN_CURLY_BRACKET STATELIST CLOSE_CURLY_BRACKET |
-            BREAK SEMICOLON | SEMICOLON)
-    """
-
+   """STATEMENT : '(' VARDECL SEMICOLON '|' ATRIBSTAT SEMICOLON '|' PRINTSTAT SEMICOLON '|' READSTAT SEMICOLON '|' RETURNSTAT SEMICOLON '|' IFSTAT '|' FORSTAT '|' OPEN_CURLY_BRACKET STATELIST CLOSE_CURLY_BRACKET '|' break SEMICOLON '|' SEMICOLON ')' """
+   pass
 
 def p_VARDECL(p):
-    """
-    VARDECL: ( int | float | string ) IDENT (OPEN_SQUARE_BRACKET INT_CONSTANT CLOSE_SQUARE_BRACKET)∗
-    """
+    """VARDECL : '(' int '|' float '|' string ')' IDENT '(' OPEN_SQUARE_BRACKET INT_CONSTANT CLOSE_SQUARE_BRACKET ')' '∗' """
+    pass
 
 def p_ATRIBSTAT(p):
-    """
-    ATRIBSTAT: LVALUE ASSIGN ( EXPRESSION | ALLOCEXPRESSION | FUNCCALL)
-    """
+    """ATRIBSTAT : LVALUE ASSIGN '(' EXPRESSION '|' ALLOCEXPRESSION '|' FUNCCALL ')' """
+    pass
 
 def p_FUNCCALL(p):
-    """
-    FUNCCALL: IDENT OPEN_PAREN PARAMLISTCALL CLOSE_PAREN)
-    """
+    """FUNCCALL : IDENT OPEN_PAREN PARAMLISTCALL CLOSE_PAREN """
+    pass
 
 def p_PARAMLISTCALL(p):
-    """
-    PARAMLISTCALL: (IDENT COMMA PARAMLISTCALL | IDENT)?
-    """
+    """PARAMLISTCALL : '(' IDENT COMMA PARAMLISTCALL '|' IDENT ')' '?' """
+    pass
 
 def p_PRINTSTAT(p):
-    """
-    PRINTSTAT: print EXPRESSION
-    """
+    'PRINTSTAT : print EXPRESSION'
+    pass
 
 def p_READSTAT(p):
-    """
-    READSTAT: read LVALUE
-    """
+    """READSTAT : read LVALUE"""
+    pass
 
 def p_RETURNSTAT(p):
-    """
-    RETURNSTAT: return
-    """
+    """RETURNSTAT : return"""
+    pass
 
 def p_IFSTAT(p):
-    """
-    IFSTAT: if OPEN_PAREN EXPRESSION CLOSE_PAREN STATEMENT (ELSE STATEMENT)?
-    """
+    """IFSTAT : if OPEN_PAREN EXPRESSION CLOSE_PAREN STATEMENT '(' else STATEMENT ')' '?' """
+    pass
 
 def p_FORSTAT(p):
-    """
-    FORSTAT: FOR OPEN_PAREN ATRIBSTAT; EXPRESSION; ATRIBSTAT CLOSE_PAREN STATEMENT
-    """
+    """FORSTAT : for OPEN_PAREN ATRIBSTAT SEMICOLON EXPRESSION SEMICOLON ATRIBSTAT CLOSE_PAREN STATEMENT"""
+    pass
 
 def p_STATELIST(p):
-    """
-    STATELIST: STATEMENT (STATELIST)?
-    """
+    "STATELIST : STATEMENT '(' STATELIST ')' '?'"
+    pass
 
 def p_ALLOCEXPRESSION(p):
-    """
-    ALLOCEXPRESSION:  new (int | float | string) (OPEN_SQUARE_BRACKET NUMEXPRESSION CLOSE_SQUARE_BRACKET)+
-    """
+    """ALLOCEXPRESSION :  new '(' int '|' float '|' string ')' '(' OPEN_SQUARE_BRACKET NUMEXPRESSION CLOSE_SQUARE_BRACKET ')' '+'  """
+    pass
 
 def p_EXPRESSION(p):
-    """
-    p_EXPRESSION: NUMEXPRESSION(( LOWER | HIGHER | LOWER_EQUAL | HIGHER_EQUAL | EQUAL | NOT_EQUAL) NUMEXPRESSION)?
-    """
+    """EXPRESSION : NUMEXPRESSION '(' '(' LOWER '|' HIGHER '|' LOWER_EQUAL '|' HIGHER_EQUAL '|' EQUAL '|' NOT_EQUAL ')' NUMEXPRESSION ')' '?' """
+    pass
 
 def p_NUMEXPRESSION(p):
-    """
-    NUMEXPRESSION:TERM ((PLUS | MINUS) TERM)∗
-    """
-
+    """ NUMEXPRESSION : TERM '(' '(' PLUS '|' MINUS ')' TERM ')' '∗' """
+    pass
+#
 def p_TERM(p):
-    """
-    TERM: UNARYEXPR(( MULTIPLY | DIVIDE | MODULUS) UNARYEXPR)∗
-    """
+    """TERM : UNARYEXPR '(' '('  MULTIPLY '|' DIVIDE '|' MODULUS ')' UNARYEXPR ')' '∗' """
+    pass
 
 def p_UNARYEXPR(p):
-    """
-    UNARYEXPR: ((PLUS | MINUS))? FACTOR
-    """
+    """UNARYEXPR : '(' '(' PLUS '|' MINUS ')' ')' '?' FACTOR """
+    pass
 
 def p_FACTOR(p):
-    """
-    FACTOR:  (INT_CONSTANT | FLOAT_CONSTANT | STRING_CONSTANT | NULL | | LVALUE | OPEN_PAREN NUMEXPRESSION CLOSE_PAREN )
-    """
+    """FACTOR :  '(' INT_CONSTANT '|' FLOAT_CONSTANT '|' STRING_CONSTANT '|' null '|' '|' LVALUE '|' OPEN_PAREN NUMEXPRESSION CLOSE_PAREN ')' """
+    pass
 
 def p_LVALUE(p):
-    """
-    p_LVALUE: IDENT( OPEN_SQUARE_BRACKET NUMEXPRESSION CLOSE_SQUARE_BRACKET )∗
-    """
+    """LVALUE : IDENT '(' OPEN_SQUARE_BRACKET NUMEXPRESSION CLOSE_SQUARE_BRACKET ')' '∗'"""
+    pass
 
 def p_error(p):
     if p:
@@ -147,4 +116,7 @@ def p_error(p):
 
 
 # Build the parser
-parser = yacc.yacc()
+try:
+    parser = yacc.yacc(debug=True)
+except:
+    print('Yacc error')
